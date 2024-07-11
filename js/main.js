c68fa2fb3d3c58d35f1b92563366365db9835868
Lucila <79485632+malucila@users.noreply.github.com>
2024-07-10 21:43:30 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+ 
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+    function validarEnviar(){
+    parrafo = document.getElementById("error")
+    // ANALIZO EL NOMBRE
+    if(document.formulario.nombre.value.length <= 2){
+        // alert("Ingrese un nombre correcto.")
+        document.formulario.nombre.focus()
+        parrafo.innerHTML = "Nombre incorrecto."
+        return
+    }
+     // ANALIZO EL APELLIDO
+     if(document.formulario.apellido.value.length <= 2){
+        // alert("Ingrese un nombre correcto.")
+        document.formulario.apellido.focus()
+        parrafo.innerHTML = "Apellido incorrecto."
+        return
+    }
+   
+    let edadEntera = parseInt(document.formulario.edad.value)
+     
+    if(isNaN(edadEntera)){
+        // alert("Tiene que ingresar un número valido en la edad.")
+        document.formulario.edad.focus()
+        parrafo.innerHTML = "Edad incorrecta."
+        return
+    }
+    else{
+        if(edadEntera < 18 || edadEntera > 65){
+            // alert("Tiene que ser mayor de edad o tener menos de 65 años.")
+            document.formulario.edad.focus()
+            parrafo.innerHTML = "Edad incorrecta."
+            return 
+        }
+    } 
+   
+    // ANALIZO SI SELECCIONÓ DE FORMA CORRECTA
+   /*  if(document.formulario.interes.selectedIndex == 0){
+        // alert("Debe seleccionar un motivo de su contacto.")
+        document.formulario.interes.focus()
+        parrafo.innerHTML = "Seleccion incorrecta."
+        return 
+    } */
+
+    parrafo.innerHTML = "Gracias por completar el formulario!"
+    document.formulario.submit()
+}
